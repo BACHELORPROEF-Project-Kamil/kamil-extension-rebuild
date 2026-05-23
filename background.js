af05a5e52bf6a87e0211308d31dff5594760697f
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2026-05-23 14:05:13 +0200

    refactor: background worker better error handling

--- background.js
+++ background.js
@@ -5,22 +5,39 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 		console.log("Tab updated:", tab.url);
 
 		// Make a post request to backend to check if the URL is safe
-		fetch("http://localhost:5001/check-url", {
+		fetch("http://localhost:5001/api/check-url", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
 			},
 			body: JSON.stringify({ url: tab.url }),
 		})
-			.then((res) => res.json())
+            // Async function to handle the response and check for errors
+			.then(async (res) => {
+				if (!res.ok) {
+					const contentType = res.headers.get("content-type");
+					if (contentType && contentType.includes("application/json")) {
+						const errorData = await res.json();
+						throw new Error(`Server error: (${res.status}): ${JSON.stringify(errorData)}`);
+					} else {
+						const errorText = await res.text();
+						throw new Error(`Server Error: (${res.status}): ${errorText.substring(0, 100)}...`);
+					}
+				}
+
+				return res.json();
+			})
 			.then((data) => {
-				if (data.safe === "unsafe") {
+				if (data.status === "unsafe") {
 					// Alert the user about the unsafe URL
 					// SOON TO BE REPLACE WITH A REAL EXTENSION POPUP ALERT
 					console.log("Unsafe URL detected: " + data.reason);
 					// Redirect to educational page
 					chrome.tabs.update(tabId, { url: data.redirectUrl });
 				}
+			})
+			.catch((err) => {
+				console.error("Error checking URL: ", err);
 			});
 	}
 });
