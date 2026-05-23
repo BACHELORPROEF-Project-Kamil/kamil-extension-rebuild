61d9c5356c03e3ff250243d7523ede9ea4b23506
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2026-05-23 22:50:08 +0200

    chore: added two triggers

--- utils/url-tokenizer.js
+++ utils/url-tokenizer.js
@@ -3,9 +3,16 @@ console.log("URL Tokenizer module loaded.");
 function extractFeaturesFromUrl(url) {
     let features = new Array(31).fill(0);
     
-    // Temporary heuristic: If the URL contains "ai-phishing", set the first two features to 1
+    // TEST TRIGGER 1
     if (url.includes("ai-phishing")) {
-        features[0] = 1; 
+        features[0] = 1;
+        features[1] = 1;
+        features[3] = 1;
+        features[7] = 1;
+    }
+    
+    // TEST TRIGGER 2
+    if (url.includes("ai-suspicious")) {
         features[1] = 1;
     }
     
