3e67c00c9b6cbc4cbf374e4cdf99ad72ef5a0395
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2026-05-30 21:50:16 +0200

    chore: added blogUrl

--- utils/scenarios.js
+++ utils/scenarios.js
@@ -4,27 +4,28 @@ const scenarios = Object.freeze({
 		title: "Alles is veilig.",
 		reason: "Geen bedreigingen gevonden.",
 		description: "Kamil heeft deze website gecontroleerd. Je kunt hier veilig surfen.",
-		blogUrl: "#",
+		blogUrl: "http://localhost:3000/tips-and-guides",
 	},
-    BLACKLISTED_URL: {
-        status: "critical",
-        title: "Gevaar: Zwarte lijst.",
-        reason: "URL staat op een zwarte lijst.",
-        description: "Deze website die je probeerde te betreden staat op een zwarte lijst van bekende phishing-websites.",
-        blogUrl: "#",
-    },
-    PUNYCODE: {
-        status: "critical",
-        title: "Gevaar: Vermomde URL.",
-        reason: "Punycode gedetecteerd.",
-        description: "Deze website gebruikt speciale tekens om te lijken op een bekende website. Dit is bijna altijd phishing.",
-        blogUrl: "#",
-    },
-    AI_PREDICTION_HIGH_RISK: {
-        status: "warning",
-        title: "Opgelet: Verdacht.",
-        reason: "AI-model meldt hoog risico.",
-        description: "Kamil's AI-model herkent patronen op deze pagina die sterk lijken op phishing. Wees extra voorzichtig.",
-        blogUrl: "#",
-    }
+	BLACKLISTED_URL: {
+		status: "critical",
+		title: "Gevaar: Zwarte lijst.",
+		reason: "URL staat op een zwarte lijst.",
+		description: "Deze website die je probeerde te betreden staat op een zwarte lijst van bekende phishing-websites.",
+		blogUrl: "http://localhost:3000/tips-and-guides/gids/zwarte-lijsten-en-databases",
+	},
+	PUNYCODE: {
+		status: "critical",
+		title: "Gevaar: Vermomde URL.",
+		reason: "Punycode gedetecteerd.",
+		description: "Deze website gebruikt speciale tekens om te lijken op een bekende website. Dit is bijna altijd phishing.",
+		blogUrl: "http://localhost:3000/tips-and-guides/gids/gevaarlijke-tekens-punycode",
+	},
+	AI_PREDICTION_HIGH_RISK: {
+		status: "warning",
+		title: "Opgelet: Verdacht.",
+		reason: "AI-model meldt hoog risico.",
+		description: "Kamil's AI-model herkent patronen op deze pagina die sterk lijken op phishing. Wees extra voorzichtig.",
+		blogUrl: "http://localhost:3000/tips-and-guides",
+		//TODO: ADD BLOG ON WEBSITE EXPLAINING AI PREDICTIONS
+	},
 });
