8a071436dc3d545382582adc5dea9a70bc622ad8
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2026-05-30 17:11:41 +0200

    chore: added scenarios

---
+++ utils/scenarios.js
@@ -0,0 +1,30 @@
+const scenarios = Object.freeze({
+	SAFE: {
+		status: "safe",
+		title: "Alles is veilig.",
+		reason: "Geen bedreigingen gevonden.",
+		description: "Kamil heeft deze website gecontroleerd. Je kunt hier veilig surfen.",
+		blogUrl: "#",
+	},
+    BLACKLISTED_URL: {
+        status: "critical",
+        title: "Gevaar: Zwarte lijst.",
+        reason: "URL staat op een zwarte lijst.",
+        description: "Deze website die je probeerde te betreden staat op een zwarte lijst van bekende phishing-websites.",
+        blogUrl: "#",
+    },
+    PUNYCODE: {
+        status: "critical",
+        title: "Gevaar: Vermomde URL.",
+        reason: "Punycode gedetecteerd.",
+        description: "Deze website gebruikt speciale tekens om te lijken op een bekende website. Dit is bijna altijd phishing.",
+        blogUrl: "#",
+    },
+    AI_PREDICTION_HIGH_RISK: {
+        status: "warning",
+        title: "Opgelet: Verdacht.",
+        reason: "AI-model meldt hoog risico.",
+        description: "Kamil's AI-model herkent patronen op deze pagina die sterk lijken op phishing. Wees extra voorzichtig.",
+        blogUrl: "#",
+    }
+});
